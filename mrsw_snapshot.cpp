9fe28b9f4e20f6c6e2e49b001c5822119f001f9b
ercagarwal <sharan.n21@gmail.com>
2020-11-10 19:19:27 +0530

    changes

--- mrsw_snapshot.cpp
+++ mrsw_snapshot.cpp
@@ -10,6 +10,7 @@
 using namespace std;
 
 #define MAX_THREADS 100
+int default_int_array[] = {-1};
 
 void checkpoint(){
 
@@ -27,7 +28,11 @@ void copy_b_to_a(int a[], int b[], int size){
 }
 
 
-
+double preprocess_timestamp(double time){ 
+    //truncates the leading 8 digits in the timestamp for clarity
+    double base = (double)((int)time/1000)*1000; //truncates the 10s, 100s, 1000s place and floating points to 0
+    return(time-base); 
+}
 
 
 class snap_value
@@ -37,7 +42,6 @@ public:
   int value;
   int label;
   int snap[MAX_THREADS];
-  
 
   snap_value(int n) //construction for dummy snap value
   {
@@ -61,8 +65,6 @@ public:
     }
     
   }
-
-  
 };
 
 
@@ -71,22 +73,23 @@ class mrsw_snapshot_obj
 {
 public:
   
-
   int n_threads;
-
   atomic<snap_value> s_table_snap_values[MAX_THREADS];
 
+  //other vars for diagnostics
+  int collisions_faced[MAX_THREADS];
 
   mrsw_snapshot_obj(int n, float u_1, float u_2)
   {
 
     n_threads = n;
-    
     snap_value dummy_sv(n_threads);
 
     for (int i = 0; i < n_threads; i++)
     {
       s_table_snap_values[i].store(dummy_sv);
+      collisions_faced[i] = 0;
+
     }
   }
 
@@ -95,7 +98,7 @@ public:
     
     // get clean snap
     int clean_snap[n_threads];
-    scan(me, clean_snap);
+    scan(clean_snap);
     
     snap_value old_snap_value = s_table_snap_values[me].load();
     snap_value new_sv(n_threads, clean_snap, new_value, old_snap_value.label+1);
@@ -106,15 +109,13 @@ public:
 
 
 
-  void scan(int me ,int clean_scan[]){
+  void scan(int clean_scan[], bool is_scanner=false){
 
     
+    
     int new_copy[n_threads], old_copy[n_threads];
-
-    //get old collect
-    collect(old_copy);
-
     bool moved[n_threads];
+    collect(old_copy);
     
     for (int i = 0; i < n_threads; i++)
     {
@@ -133,13 +134,16 @@ public:
         if(old_copy[i] != new_copy[i]){
           
           if(moved[i]){ //second move, get snap  of this register
-            
+            cout << "found second move !" << endl;
             copy_b_to_a(clean_scan, s_table_snap_values[i].load().snap, n_threads);
+            
             return;
           }
           else{
             moved[i] = true;
+            cout << "someone moved!" << endl;
             copy_b_to_a(old_copy, new_copy, n_threads); // old_copy = new_copy
+            
             break;
           }
         }
@@ -157,13 +161,14 @@ public:
     {
       collected[i] = s_table_snap_values[i].load().value;
     }
+    usleep(1000);
     return;
   }
 
 
   void display()
   {
-    cout << "------------------printing state of object-----------------" << endl;
+    cout << endl << "------------------printing state of object-----------------" << endl;
 
     cout << "No of MRSW registers/threads: " << n_threads << endl;
 
@@ -178,7 +183,6 @@ public:
       cout << "has snap: " << endl;
       for (int j = 0; j < n_threads; j++)
       {
-        // cout << j << endl;
         cout << s_table_snap_values[i].load().snap[j] << ", "; 
       }
       cout << endl << "-------------------------------------------" << endl;
@@ -189,11 +193,34 @@ public:
 };
 
 
+void write_to_file(fstream &of, int n, int id = -1, int snapshot[] = default_int_array, double time_now = -1, int value = -1, int k = -1,  bool is_snapshot = false){
+
+  #pragma omp critical
+  {
+    if(is_snapshot){
+      of << endl << endl;
+      of << "got " << k << "th snapshot at timestamp: "<< time_now << " as:" << endl;
+
+      for (int i = 0; i < n; i++)
+      {
+       of << snapshot[i] << ", ";  
+      }
+      of << endl << endl;
+      
+    }
+    else{
+      of << "thread " << id << " wrote a value: "<< value << " at timestamp: " << time_now << endl;
+
+    }
+  }
+  
+}
+
+
 
 int main()
 {
   srand(time(NULL)); //set random seed using time for future random numbers generated
-
 
   double N, count = 0;
   double start_time, end_time;
@@ -202,11 +229,12 @@ int main()
 
   // init input and output files, get input parameters
   ifstream input_file;
-  fstream times_file;
-  fstream output_file;
+  fstream writers_file;
+  fstream snapshots_file;
+
   input_file.open("inp-params.txt", ios::in);
-  times_file.open("Times.txt", ios::app);
-  output_file.open("Primes-SAM1.txt");
+  writers_file.open("writers_file.txt");
+  snapshots_file.open("snapshots_file.txt");
 
   input_file >> n; // no of writer threads
   input_file >> m;
@@ -214,48 +242,61 @@ int main()
   input_file >> u_2;
   input_file >> k;
 
-
   mrsw_snapshot_obj ss(n, u_1, u_2);
-
 
   start_time = omp_get_wtime();
   omp_set_num_threads(n + 1);
 
-
 #pragma omp parallel
   {
-    int id, i = 0;
+    int id, no_of_writes = 0, no_of_snapshots = 0, rand_val;
+    double time_now;
+
     id = omp_get_thread_num();
 
     if (id == n) //snapshot collecting thread executes here
     { 
       
-      
+      int clean_snap[n];
+
+      snapshots_file << "n: " << n << endl;
+      snapshots_file << "k: " << k << endl;
+      snapshots_file << "u_1: " << u_1 << endl;
+      snapshots_file << "u_2: " << u_2 << endl;
+
+      while(no_of_snapshots < k){
+
+        ss.scan(clean_snap, true);
+        time_now = preprocess_timestamp(omp_get_wtime());
+
+        write_to_file(snapshots_file, n, -1, clean_snap, time_now, -1, no_of_snapshots, true);
+        
+        no_of_snapshots++;
+      }
     }
 
     else //writing thread executes her
     {
 
-      while (i < 10)
+      while (no_of_writes < 10)
       {
-  
-        i++;
-        int rand_val = rand() % 100;
-        
-        
+
+        no_of_writes++;
+        rand_val = rand() % 100; 
         ss.update(id, rand_val);
 
-        #pragma omp critical
-        {
-          cout << "Thread: " << id << " wrote value: " << rand_val << " at: " << endl;
-        }
-        usleep(100000);
+        time_now = preprocess_timestamp(omp_get_wtime());
+
+        write_to_file(snapshots_file, n, id, nullptr, time_now, rand_val, -1, false);
+        usleep(100);
 
       }
     }
   }
 
-  ss.display();
+
+
+  // ss.display();
   exit(0);
 
   // stop timer
@@ -263,6 +304,6 @@ int main()
 
   // close files
   input_file.close();
-  times_file.close();
-  output_file.close();
+  writers_file.close();
+  snapshots_file.close();
 }
